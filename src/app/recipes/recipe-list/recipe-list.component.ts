2abef76c8a39a0eed291b5f61286416ac1973bb3
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2021-07-03 16:11:51 -0400

    added data to recipe and shopping-list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is just a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqqxHemGDCQK4ulOWZyecL4hqsJTVnibRInQ&usqp=CAU'),
+    new Recipe('Test Recipe 2', 'This is another test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqqxHemGDCQK4ulOWZyecL4hqsJTVnibRInQ&usqp=CAU')
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+    constructor(public name: string, public amount: number) {}
+}
+
+// this is a shortcut as opposed to the recipe model
